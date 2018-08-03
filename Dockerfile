c8a6adaa440f7fc5ab0fae683455a78ecb3b3215
SCHWARTZ Anthony <anthony.schwartz@ext.ec.europa.eu>
2018-08-03 12:26:02 +0200

    Adding workir

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM alpine:3.7
 
-ENV DOCUMENT_ROOT /var/www/html
 ENV SUPERVISORD_CONF_DIR /etc/supervisord/
 ENV DAEMON_USER "node"
 ENV DAEMON_GROUP "node"
+ENV WORKDIR /opt/nodejs
+
+WORKDIR $WORKDIR
 
 ### Add ssmtp, nodejs, bash, git & upgrade npm
 RUN echo @edge http://nl.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories && echo @edge http://nl.alpinelinux.org/alpine/edge/main >> /etc/apk/repositories && apk add --no-cache ssmtp nodejs bash git chromium@edge nss@edge && npm i npm@latest -g && npm i supervisor && sed -ri 's@^mailhub=mail$@mailhub=127.0.0.1@' /etc/ssmtp/ssmtp.conf
--- run.sh
+++ run.sh
@@ -2,28 +2,25 @@
 set -e
 
 # Get uid for the current docroot
-[ -z "${DOCUMENT_ROOT}" ] && export DOCUMENT_ROOT=/var/www/html
+[ -z "${WORKDIR}" ] && export WORKDIR=/var/www/html
 
 # Check if docroot or parent exists :
-[ -d "${DOCUMENT_ROOT}" ] && export REF_DIR=${DOCUMENT_ROOT}
+[ -d "${WORKDIR}" ] && export REF_DIR=${WORKDIR}
 # Else use its parent
-[ -z "${REF_DIR}" ] && export REF_DIR=$(dirname ${DOCUMENT_ROOT})
+[ -z "${REF_DIR}" ] && export REF_DIR=$(dirname ${WORKDIR})
 
 # Get our command to run
 export CMD="${@}"
-
-# If APACHE_EXTRA_CONF isn't being set outside, set it to an empty value.
-if [ -z ${APACHE_EXTRA_CONF} ]; then export APACHE_EXTRA_CONF=""; fi
 
 if [ -z "${CMD}" ]; then
   # If no run command provided, run supervisor as root a:
   /usr/bin/supervisord -c /etc/supervisord/supervisord.conf
 else
   # Run the command as user web
-  if ! `grep -q www-data /etc/passwd`
+  if ! `grep -q nodejs /etc/passwd`
   then
     eval "${CMD}"
   else
-    HOME=/tmp su -s /bin/bash -c "${CMD}" www-data
+    HOME=/tmp su -s /bin/bash -c "${CMD}" nodejs
   fi
 fi
